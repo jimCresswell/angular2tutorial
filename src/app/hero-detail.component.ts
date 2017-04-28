114b731ffbf4a8a693deb0cc035a1384102c7c9c
jimCresswell <jim.cresswell@gmail.com>
2017-04-28 08:40:31 +0100

    In memory DB and CRUD

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -15,17 +15,25 @@ import 'rxjs/add/operator/switchMap';
 
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
   ) {};
+
   ngOnInit(): void {
   this.route.params
     .switchMap((params: Params) => this.heroService.getHero(+params['id']))
     .subscribe(hero => this.hero = hero);
   }
+
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
   }
 }
